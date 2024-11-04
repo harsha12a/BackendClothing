67880c2a503e78549376590398bd55bd4506bfca
harha12a <harsha.pss2005@gmail.com>
2024-11-04 11:01:12 +0530

    add data

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const userApp = require('./routes/userRoute');
 const watchApp = require('./routes/watchRoute');
 const hatApp = require('./routes/hatRoute');
 const shoeApp = require('./routes/shoeRoute');
+const menApp = require('./routes/menRoute');
+const womenApp = require('./routes/womenRoute');
 
 let app = express();
 require('dotenv').config();
@@ -16,6 +18,10 @@ mClient.connect().then((connectobj)=>{
     const watches = clothstore.collection('watches');
     const hats = clothstore.collection('hats');
     const shoes = clothstore.collection('shoes');
+    const men = clothstore.collection('men');
+    const women = clothstore.collection('women');
+    app.set('men',men)
+    app.set('women',women)
     app.set('users',users)
     app.set('watches',watches)
     app.set('hats',hats)
@@ -31,6 +37,8 @@ app.use('/user',userApp)
 app.use('/watch',watchApp)
 app.use('/hat',hatApp)
 app.use('/shoe',shoeApp)
+app.use('/men',menApp)
+app.use('/women',womenApp)
 
 app.use('*',(req,res,next)=>{
     res.send('404 not found')
