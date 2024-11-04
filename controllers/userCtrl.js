4ad883126921213e9ccb71196662ddbf8ed5e488
harha12a <harsha.pss2005@gmail.com>
2024-11-05 04:21:17 +0530

    add:createUser

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,10 +1,17 @@
 const expasync = require('express-async-handler')
+const bcrypt = require('bcrypt');
 
 const createuser = expasync(async(req,res) =>{
     let user = req.app.get('users')
     let newUser = req.body
-    
-    console.log(newUser)
+    let result = await user.findOne({username:newUser.username})
+    if(result) res.send({message:'User already exists'})
+    else{
+        let newPass = newUser.password
+        newUser.password = await bcrypt.hash(newPass,10)
+        let response = await user.insertOne(newUser)
+        res.send({message:response,paylad:newUser})
+    }
 })
 
 const getuser = expasync(async(req,res) =>{
